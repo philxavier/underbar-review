6a104f4c3121749ad669136846af51637502d433
John Doe <jason.andre.bourget@gmail.com>
2018-09-28 20:53:30 -0600

    completed through reject

--- src/underbar.js
+++ src/underbar.js
@@ -88,10 +88,10 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    return _.filter(collection, function(element) {
+      return !test(element);
+    });
   };
-
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
   };
